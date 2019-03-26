71e800e4acdc2b97f4a4c9602460a84ef0b1143d
Kevin Desousa <kevin.desousa1@uoit.net>
2019-03-25 20:32:06 -0400

    Added more functionality to the table selection feature

--- client/js/driver.js
+++ client/js/driver.js
@@ -1,43 +1,89 @@
 $(document).ready(function () {
+    // Get the current driver's name
     let driver_name = $("#driver-name").text();
 
-    // Append the driver picture
     if (driver_name == "New Driver" || driver_name == "Driver Not Found") {
         // Default image here
     } else {
+       // Append the driver picture
         getPicUrl(driver_name, function (data) {
             $("<img class='driver-portrait driver-thumbnail' src=" + data.items[0].link + ">").appendTo($("#driver-image"));
         });
 
-
         $.ajax({
             url: "/driverdetails",
-            method: "POST", 
-            data: {"forename": driver_name.split(" ")[0], 
-                    "surname":driver_name.split(" ")[1] },
-            success: function(result){
+            method: "POST",
+            data: {
+                "forename": driver_name.split(" ")[0],
+                "surname": driver_name.split(" ")[1]
+            },
+            success: function (result) {
+                // Get the driver's results
                 let driver_data = result[0];
-                console.log(driver_data);
 
+                // Add the driver's data into the table
                 let table = $(".table");
-                for(let col in driver_data){
+                for (let col in driver_data) {
                     let row = $("<tr></tr>");
                     $("<th></th>").text(col).appendTo(row);
                     $("<td></td>").text(driver_data[col]).appendTo(row);
-                    row.appendTo(table);    
+                    row.appendTo(table);
                 }
 
-                $(".table").mousedown(function(event){
-                    let target = $(event.target)
-                    removeSelected($(".table"));
+                // Allow the rows to be edited
+                $(".table").click(function (event) {
+                    // Get the clicked element
+                    let target = $(event.target);
 
-                    if(target.prop("tagName") == "TD"){
-                        let confirmButton = $('<input type="button" value="confirm" class="confirm-btn"/>')
-                        target.attr('contenteditable', true)
-                        target.append(confirmButton)
+                    // Remove all the previously selected rows
+                    removeSelected($(".table"), $(target.parent()), true);
+
+                    // If the row is not already selected
+                    if (target.prop("tagName") == "TD" && !event.target.isContentEditable) {
+                        // Get the original text
+                        let original_content = target.text();
+                        // Make a button to confirm the row edit
+                        let confirm_button = $('<input type="button" value="confirm" class="f1-font confirm-btn"/>');
+
+                        // Set the column to be editable
+                        target.attr('contenteditable', true);
+                        // Focus on the newly editable content
+                        target.focus();
+                        // Set the id of the td to the previous val
+                        target.prop("id", original_content);
+
+                        // On click, change the value of the button and remove the id
+                        confirm_button.click(function (event) {
+                            removeSelected($(".table"), $($(event.target).parent()));
+                        });
+                        // Append the button to the row
+                        confirm_button.appendTo($(target.parent()));
                     }
                 })
             }
         });
     }
 });
+
+function removeSelected(element, target_parent, revert_val) {
+    // Get all the contenteditable tds
+    let editable_tds = element.find("td[contenteditable='true']");
+    for (let i = 0; i < editable_tds.length; i++) {
+        // Get the specific td
+        let td = $(editable_tds[i]);
+
+        // If the td is not the same as the current one or we are updating the value
+        if (!$(td.parent()).is(target_parent) || !revert_val) {
+            // If we are reverting the value copy the id back into the element
+            if (revert_val) {
+                let original_val = td.prop("id");
+                td.text(original_val);
+            }
+            // Remove the id and the contenteditable attributes
+            td.removeAttr('id');
+            td.removeAttr('contenteditable');
+            // Remove the confirm button
+            $(td.parent()).find(".confirm-btn").remove();
+        }
+    }
+}
--- client/js/extra.js
+++ client/js/extra.js
@@ -2,14 +2,13 @@
  * This class is used to store functions that are required by many 
  * functions on the site`
  */
-
 let google_key = "AIzaSyBphDItx-Lx0JtGqGfPmgg3BV34iLiN08s";
 
 function getPicUrl(query, func) {
 
     // Get the image of the driver from google
     $.get({
-        url: "https://www.googleapis.com/customsearch/v1?key=" + google_key + "&cx=016545330576617661627:07nzt7b2mne&searchType=image&q=" +query,
+        url: "https://www.googleapis.com/customsearch/v1?key=" + google_key + "&cx=016545330576617661627:07nzt7b2mne&searchType=image&q=" + query,
         success: function (google_result) {
             func(google_result);
         }
@@ -17,13 +16,3 @@ function getPicUrl(query, func) {
         // async: true
     });
 }
-
-function removeSelected(element){
-    let td = element.find("td");
-    for(let i = 0; i <td.length; i++){
-        // makes element not editable
-        $(td[i]).attr('contenteditable', 'false');
-        $(td[i]).remove(".confirm-btn")
-    }
-}
-
